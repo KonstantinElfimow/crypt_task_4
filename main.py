6cdb1ca086dbe61d6b6ea10f367702af62e2010e
Konstantin <k.elfimow@gmail.com>
2022-11-14 22:10:02 +0300

    Была проблема с возведением числа в степень по модулю. Уже исправлено

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def euler_phi(n: int) -> int:
+    """ Функция Эйлера Fn = (p - 1) * (q - 1), n = p * q. """
     result = n
     i = 2
     while i * i <= n:
@@ -10,7 +11,7 @@ def euler_phi(n: int) -> int:
             i += 1
     if n > 1:
         result -= result // n
-    print("euler: ", result)
+    # print("euler: ", result)
     return result
 
 
@@ -22,7 +23,7 @@ def egcd(a, b) -> (int, int, int):
         return g, x - (b // a) * y, y
 
 
-def find_secret_key(e: int, Fn: int) -> int:
+def find_secret_key(e: int, Fn: int) -> int | None:
     gcd, x, y = egcd(e, Fn)
     if gcd != 1:
         return None
@@ -44,7 +45,7 @@ def parse_cipher_text(cipher_text: str, n: int) -> list:
             i += len(str_n) - 1
     result.append(cipher_text[i: len(cipher_text)])
 
-    print("Распарсенный С: ", result)
+    # print("Распарсенный С: ", result)
     return result
 
 
@@ -52,10 +53,10 @@ def decrypt_message(lst_cipher: list, d: int, n: int) -> str:
     decrypted: str = ''
     for c in lst_cipher:
         m = pow(int(c), d, n)
-        print(m)
+        # print(m)
         decrypted += str(m)
 
-    print("Исходное М (int): ", decrypted)
+    # print("Исходное М (int): ", decrypted)
 
     result: str = ''
     for i in range(0, len(decrypted), 2):
@@ -74,8 +75,9 @@ def RSA_decrypt(cipher_text: str, e: int, n: int) -> bool:
     if d is None:
         print("Были переданы неверные значения!")
         return False
-    print("Ключ верный? ", test_key(d, e, Fn))
-    print("d: ", d)
+
+    # print("Ключ верный? ", test_key(d, e, Fn))
+    # print("d: ", d)
 
     parsed_cipher: list = parse_cipher_text(cipher_text, n)
 
@@ -86,6 +88,7 @@ def RSA_decrypt(cipher_text: str, e: int, n: int) -> bool:
 
 
 def main():
+    # Входная данные
     cipher_text: str = "6073334549854737481838817505469218919893372481934272786799829052850710603231301937207488228"
     e: int = 11119
     n: int = 1814354438978629
